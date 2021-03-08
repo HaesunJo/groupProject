b60391fe10b4060403aff2eef0a6a21bb77fa972
johaesun <johaesun@haillies-mbp.hitronhub.home>
2021-03-07 22:11:08 -0800

    added more rows

--- GProjectDemo/src/main/java/com/example/GProjectDemo/GProjectDemoApplication.java
+++ GProjectDemo/src/main/java/com/example/GProjectDemo/GProjectDemoApplication.java
@@ -27,22 +27,29 @@ public class GProjectDemoApplication {
 			ManagerRepository managerRepository, ProductRepository productRepository) {
 		return args -> {
 			
-			FitnessClass[] classes = {new FitnessClass("Yoga",1, "Jan",2,"Monday", "12:30","Tiffany", "Vancouver")
+			FitnessClass[] classes = {new FitnessClass("Yoga",1, "Jan",2,"Monday", "12:30","Tiffany", "Vancouver"),
+					new FitnessClass("Pilates",1, "Feb",5,"Friday", "12:30","Tiffany", "Vancouver"),
+					new FitnessClass("Yoga",2, "Jan",2,"Monday", "12:30","Jason", "Richimnond"),
+					new FitnessClass("Pilates",2, "Feb",16,"Tuesday", "12:30","Tiffany", "Vancouver"),
+					new FitnessClass("PT",1, "Jan",2,"Monday", "11:30","Emile Imanov", "Vancouver")
 					
 			};
 			
 			Customer[] customers = {new Customer("1234", "Seunghee Ko", "kos6@student.douglascollege.ca", "1234567"),
-									new Customer("5678", "Haesun Jo", "Joh31@student.douglascollege.ca", "1234567"),
+									new Customer("5678", "Ozan Cin", "cino21@student.douglascollege.ca", "1234567"),
 									new Customer("3456", "Ivan Wong", "kos6@student.douglascollege.ca", "1234567"),
 									new Customer("1123", "Chelsea James", "cjames@gmail.com", "1234567")
 									};
 			
 
-			Manager[] managers = {	new Manager("riako", "Seunghee Ko", "kos6@student.douglascollege.ca", "1234567")
+			Manager[] managers = {	new Manager("riako", "Seunghee Ko", "kos6@student.douglascollege.ca", "1234567"),
+									new Manager("hsj31","Haesun Jo","joh31@student.douglascollege.ca","10293847")
 					
 									};
 			
-			Product[] products = {new Product("Vegetarian", 150.00)
+			Product[] products = {	new Product("Vegetarian", 150.00),
+									new Product("Protine Max", 230.00),
+									new Product("Everyday minxed nuts / 2 weeks", 40.00)
 				
 			};
 			
