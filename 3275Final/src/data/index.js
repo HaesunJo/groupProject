b8cb5cf3817afc290de5a040b80aa6810d6fac78
haillie <hsj30338@gmail.com>
2021-04-09 16:36:50 -0700

    Update index.js

--- 3275Final/src/data/index.js
+++ 3275Final/src/data/index.js
@@ -1,16 +1,46 @@
-export default [{
-        writer: 'Customer name1',
-        title: 'Title here',
-        content: '1'
-    },
-    {
-        writer: 'Customer name2',
-        title: 'Title here',
-        content: '2'
-    },
-    {
-        writer: 'Customer name3',
-        title: 'Title here',
-        content: '3'
-    }
-]
+export default {
+    User: [{
+            user_id: 1,
+            name: customer1,
+            created_at: '2021-04-03'
+        },
+        {
+            user_id: 2,
+            name: customer2,
+            created_at: '2021-05-03'
+        },
+        {
+            user_id: 3,
+            name: customer3,
+            created_at: '2021-06-03'
+        }
+    ],
+    Content: [{
+            content_id: 1,
+            user_id: 1,
+            title: 'I like the classes here',
+            context: 'Yoga class was amazing',
+            created_at: '2021-04-03',
+            updated_at: null
+
+        },
+        {
+            content_id: 2,
+            user_id: 3,
+            title: 'can I ask something?',
+            context: 'are you open on holidays?',
+            created_at: '2021-05-03',
+            updated_at: null
+
+        },
+        {
+            content_id: 3,
+            user_id: 2,
+            title: 'How much is 6 weeks meal plan?',
+            context: 'how much?',
+            created_at: '2021-04-03',
+            updated_at: null
+
+        }
+    ]
+}
